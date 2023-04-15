cf7ceff0429b21450bd99df7d12eda010fb626bd
ghaithAlMasri <almasrighaith101@gmail.com>
2023-04-15 12:43:17 +0200

    edited title

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
-import SanityClient from "@sanity/client";
+import {createClient} from "@sanity/client";
 import imageUrlBuilder from '@sanity/image-url'
 
 
-export const client = SanityClient({
+export const client = createClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
